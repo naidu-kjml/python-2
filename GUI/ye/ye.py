79a94cc7e9dc116c57da28e9aff6bc4252f95a18
gtxy <981805032@qq.com>
2019-01-29 19:48:40 +0800

    gui

---
+++ GUI/ye/ye.py
@@ -0,0 +1,84 @@
+#!//usr/bin/env python
+#-*-coding:utf-8 -*-
+import os,requests,json,socket,shutil
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+
+
+class Ye(object):
+	def __init__(self,page):
+		self.pageIndex=page
+
+	def get_link(self,url,picname):
+		socket.setdefaulttimeout(5.0)
+		header= {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
+		proxies={"http":"http//61.135.217.7", "https":"182.47.67.102"}
+		r=requests.get(url)
+		content=r.text
+		soup=BeautifulSoup(content,'lxml')
+		divs=soup.find('input',{'name':'copy_sel'})
+		link=divs.get('value')
+		#d={picname:link}
+	#	f=open(r'%s\link.json'%self.path,'a')
+	#	json.dump(d,f)
+		#f.close()
+		image=soup.find("img", {"class": "img"}).get("src")
+		try:
+			urlretrieve(image,r'%s\%s.jpg'%(self.path,picname))
+		except HttpError as e:
+			print(e.code)
+		except socket.timeout:
+			print('保存第%s页图片$s失败'%(self.pageIndex,picname))
+		with open(r'%s\link.txt'%self.path,'a') as f:
+			f.write(link+'\n')
+		print('保存第%s页图片%s成功'%(self.pageIndex, picname))
+			
+			
+	
+	def get_urllist(self):
+		url_init=r'http://www.uuzyz002.com'
+		url_list=[]
+		url=r'http://www.uuzyz002.com/juru/index-%s.html'%self.pageIndex
+		header= {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
+		proxies={"http":"http//61.135.217.7", "https":"182.47.67.102"}
+		r=requests.get(url)
+		r.encoding = 'utf-8'
+		content=r.text
+		soup=BeautifulSoup(content,'lxml')
+		tbody=soup.find_all('tbody')
+		trs=tbody[1].find_all('tr',{'bgcolor':'#f5fef7'})
+		for t in trs:
+			url={}
+			td=t.find('td')
+			i=td.find('a')
+			url['theme']=i.text
+			url['url']=url_init+i.get('href')
+			url_list.append(url)
+		return url_list
+	
+	def start(self,progressBar):
+		urllist=self.get_urllist()
+		self.path=str(self.pageIndex)
+		if os.path.exists(self.path):
+			shutil.rmtree(self.path)
+		os.mkdir(self.path)
+		picname=1
+		sum = len(urllist)
+		for l in urllist:
+			url=l['url']
+			try:
+				progressBar.setProperty("value", (100/sum)*picname)
+				if (100/sum)*picname>98:
+					progressBar.setProperty("value", 100)
+				self.get_link(url,picname)
+			except:
+				print('第%d页面不存在'%picname)
+				continue
+			picname=picname+1
+				
+				
+		#print('第%s加载完成'%self.pageIndex)
+
+if __name__=='__main__':
+	y = Ye(str(1))
+	y.start()
--- GUI/ye/ye321.py
+++ GUI/ye/ye321.py
@@ -1,47 +1,57 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPixmap
-import sys, os, re
+import sys, os, re, time
 from untitled import Ui_Form
+from ye import Ye
 
 
 class MyForm(QWidget, Ui_Form):
     def __init__(self):
         super(MyForm, self).__init__()
         self.setupUi(self)
+        self.links = []
+        if not os.path.exists(r'D:\python\GUI\ye\1'):
+            os.mkdir(r'D:\python\GUI\ye\1')
+        try:
+            with open(r'D:\python\GUI\ye\1\link.txt', 'r') as f:
+                self.links = f.readlines()
+        except:
+            pass
         # 加载图片
-        pic_list = self.piclist(r'D:\python\qsbk\img')
-        self.label_1.setPixmap(QPixmap(pic_list[0]))
-        self.label_2.setPixmap(QPixmap(pic_list[1]))
-        self.label_3.setPixmap(QPixmap(pic_list[2]))
-        self.label_4.setPixmap(QPixmap(pic_list[3]))
-        self.label_5.setPixmap(QPixmap(pic_list[4]))
-        self.label_6.setPixmap(QPixmap(pic_list[5]))
-        self.label_7.setPixmap(QPixmap(pic_list[6]))
-        self.label_8.setPixmap(QPixmap(pic_list[7]))
-        self.label_9.setPixmap(QPixmap(pic_list[8]))
-        self.label_10.setPixmap(QPixmap(pic_list[9]))
-        self.label_11.setPixmap(QPixmap(pic_list[10]))
-        self.label_12.setPixmap(QPixmap(pic_list[11]))
-        self.label_13.setPixmap(QPixmap(pic_list[12]))
-        self.label_14.setPixmap(QPixmap(pic_list[13]))
-        self.label_15.setPixmap(QPixmap(pic_list[14]))
-        self.label_16.setPixmap(QPixmap(pic_list[15]))
-        self.label_17.setPixmap(QPixmap(pic_list[16]))
-        self.label_18.setPixmap(QPixmap(pic_list[17]))
-        self.label_19.setPixmap(QPixmap(pic_list[18]))
-        self.label_20.setPixmap(QPixmap(pic_list[19]))
-        self.label_21.setPixmap(QPixmap(pic_list[20]))
-        self.label_22.setPixmap(QPixmap(pic_list[21]))
-        self.label_23.setPixmap(QPixmap(pic_list[22]))
-        self.label_24.setPixmap(QPixmap(pic_list[23]))
-        self.label_25.setPixmap(QPixmap(pic_list[24]))
-        self.label_26.setPixmap(QPixmap(pic_list[25]))
-        self.label_27.setPixmap(QPixmap(pic_list[26]))
-        self.label_28.setPixmap(QPixmap(pic_list[27]))
-        self.label_29.setPixmap(QPixmap(pic_list[28]))
-        self.label_30.setPixmap(QPixmap(pic_list[29]))
-        self.label_31.setPixmap(QPixmap(pic_list[30]))
-        self.label_32.setPixmap(QPixmap(pic_list[31]))
+        pic_list = self.piclist(r'D:\python\GUI\ye\1')
+        try:
+            self.label_1.setPixmap(QPixmap(pic_list[0]))
+            self.label_2.setPixmap(QPixmap(pic_list[11]))
+            self.label_3.setPixmap(QPixmap(pic_list[22]))
+            self.label_4.setPixmap(QPixmap(pic_list[24]))
+            self.label_5.setPixmap(QPixmap(pic_list[25]))
+            self.label_6.setPixmap(QPixmap(pic_list[26]))
+            self.label_7.setPixmap(QPixmap(pic_list[27]))
+            self.label_8.setPixmap(QPixmap(pic_list[28]))
+            self.label_9.setPixmap(QPixmap(pic_list[29]))
+            self.label_10.setPixmap(QPixmap(pic_list[1]))
+            self.label_11.setPixmap(QPixmap(pic_list[2]))
+            self.label_12.setPixmap(QPixmap(pic_list[3]))
+            self.label_13.setPixmap(QPixmap(pic_list[4]))
+            self.label_14.setPixmap(QPixmap(pic_list[5]))
+            self.label_15.setPixmap(QPixmap(pic_list[6]))
+            self.label_16.setPixmap(QPixmap(pic_list[7]))
+            self.label_17.setPixmap(QPixmap(pic_list[8]))
+            self.label_18.setPixmap(QPixmap(pic_list[9]))
+            self.label_19.setPixmap(QPixmap(pic_list[10]))
+            self.label_20.setPixmap(QPixmap(pic_list[12]))
+            self.label_21.setPixmap(QPixmap(pic_list[13]))
+            self.label_22.setPixmap(QPixmap(pic_list[14]))
+            self.label_23.setPixmap(QPixmap(pic_list[15]))
+            self.label_24.setPixmap(QPixmap(pic_list[16]))
+            self.label_25.setPixmap(QPixmap(pic_list[17]))
+            self.label_26.setPixmap(QPixmap(pic_list[18]))
+            self.label_27.setPixmap(QPixmap(pic_list[19]))
+            self.label_28.setPixmap(QPixmap(pic_list[20]))
+            self.label_29.setPixmap(QPixmap(pic_list[21]))
+            self.label_30.setPixmap(QPixmap(pic_list[23]))
+        except:
+            pass
         self.label_1.setScaledContents(True)
         self.label_2.setScaledContents(True)
         self.label_3.setScaledContents(True)
@@ -72,19 +82,96 @@ class MyForm(QWidget, Ui_Form):
         self.label_28.setScaledContents(True)
         self.label_29.setScaledContents(True)
         self.label_30.setScaledContents(True)
-        self.label_31.setScaledContents(True)
-        self.label_32.setScaledContents(True)
 
     def piclist(self, filepath):
         pic_list = []
+        j = 0
         for i in os.listdir(filepath):
-            pic_list.append(os.path.join(filepath, i))
+            if re.match('.*?\.jpg', i):
+                pic_list.append(os.path.join(filepath, i))
+                print(os.path.join(filepath, i)+str(j))
+                j+=1
         return pic_list
 
     def copy(self):
         r = re.match('复制链接(.*)', self.sender().text())
         index = int(r.group(1))
-        self.lineEdit.setText(str(index))
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.links[index-1])
+
+    def download(self):
+        self.pushButton.setDisabled(True)
+        pages = self.spinBox.text()
+        y = Ye(pages)
+        y.start(self.progressBar)
+        with open(r'D:\python\GUI\ye\1\link.txt', 'r') as f:
+            self.links = f.readlines()
+        # 加载图片
+        pic_list = self.piclist(r'D:\python\GUI\ye\1')
+        try:
+            self.label_1.setPixmap(QPixmap(pic_list[0]))
+            self.label_2.setPixmap(QPixmap(pic_list[11]))
+            self.label_3.setPixmap(QPixmap(pic_list[22]))
+            self.label_4.setPixmap(QPixmap(pic_list[24]))
+            self.label_5.setPixmap(QPixmap(pic_list[25]))
+            self.label_6.setPixmap(QPixmap(pic_list[26]))
+            self.label_7.setPixmap(QPixmap(pic_list[27]))
+            self.label_8.setPixmap(QPixmap(pic_list[28]))
+            self.label_9.setPixmap(QPixmap(pic_list[29]))
+            self.label_10.setPixmap(QPixmap(pic_list[1]))
+            self.label_11.setPixmap(QPixmap(pic_list[2]))
+            self.label_12.setPixmap(QPixmap(pic_list[3]))
+            self.label_13.setPixmap(QPixmap(pic_list[4]))
+            self.label_14.setPixmap(QPixmap(pic_list[5]))
+            self.label_15.setPixmap(QPixmap(pic_list[6]))
+            self.label_16.setPixmap(QPixmap(pic_list[7]))
+            self.label_17.setPixmap(QPixmap(pic_list[8]))
+            self.label_18.setPixmap(QPixmap(pic_list[9]))
+            self.label_19.setPixmap(QPixmap(pic_list[10]))
+            self.label_20.setPixmap(QPixmap(pic_list[12]))
+            self.label_21.setPixmap(QPixmap(pic_list[13]))
+            self.label_22.setPixmap(QPixmap(pic_list[14]))
+            self.label_23.setPixmap(QPixmap(pic_list[15]))
+            self.label_24.setPixmap(QPixmap(pic_list[16]))
+            self.label_25.setPixmap(QPixmap(pic_list[17]))
+            self.label_26.setPixmap(QPixmap(pic_list[18]))
+            self.label_27.setPixmap(QPixmap(pic_list[19]))
+            self.label_28.setPixmap(QPixmap(pic_list[20]))
+            self.label_29.setPixmap(QPixmap(pic_list[21]))
+            self.label_30.setPixmap(QPixmap(pic_list[23]))
+            self.label_1.setScaledContents(True)
+            self.label_2.setScaledContents(True)
+            self.label_3.setScaledContents(True)
+            self.label_4.setScaledContents(True)
+            self.label_5.setScaledContents(True)
+            self.label_6.setScaledContents(True)
+            self.label_7.setScaledContents(True)
+            self.label_8.setScaledContents(True)
+            self.label_9.setScaledContents(True)
+            self.label_10.setScaledContents(True)
+            self.label_11.setScaledContents(True)
+            self.label_12.setScaledContents(True)
+            self.label_13.setScaledContents(True)
+            self.label_14.setScaledContents(True)
+            self.label_15.setScaledContents(True)
+            self.label_16.setScaledContents(True)
+            self.label_17.setScaledContents(True)
+            self.label_18.setScaledContents(True)
+            self.label_19.setScaledContents(True)
+            self.label_20.setScaledContents(True)
+            self.label_21.setScaledContents(True)
+            self.label_22.setScaledContents(True)
+            self.label_23.setScaledContents(True)
+            self.label_24.setScaledContents(True)
+            self.label_25.setScaledContents(True)
+            self.label_26.setScaledContents(True)
+            self.label_27.setScaledContents(True)
+            self.label_28.setScaledContents(True)
+            self.label_29.setScaledContents(True)
+            self.label_30.setScaledContents(True)
+        except:
+            pass
+        self.pushButton.setEnabled(True)
 
 
 if __name__ == '__main__':
@@ -92,4 +179,3 @@ if __name__ == '__main__':
     MyForm = MyForm()
     MyForm.show()
     sys.exit(app.exec_())
-
