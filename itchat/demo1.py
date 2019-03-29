97d5335142c3a1427563318535dd76d2d33d427e
GTXY <981805032@qq.com>
2019-03-30 00:25:53 +0800

    实现requests登陆oa

--- itchat/demo1.py
+++ itchat/demo1.py
@@ -3,7 +3,7 @@ import itchat,time
 
 @itchat.msg_register(itchat.content.TEXT)
 def reply_msg(msg):
-	id = ['@1eaa07b4363da13f703feefdb47a45517086b1c3279d9775833d11affb6ee3cc', '@4482ad5122ff6a679f32658a6e08f4e66973d28208188b78aeea99787adb29cd1']
+	id = ['@1eaa07b4363da13f703feefdb47a45517086b1c3279d9775833d11affb6ee3cc', '@4482ad5122ff6a679f32658a6e08f4e66973d28208188b78aeea99787adb29cd']
 	if msg['FromUserName'] not in id:
 		itchat.send_msg('%s\n正在忙，稍后回复。。。'%time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), msg['FromUserName'])
 
--- spider/oa_login/oa_login.py
+++ spider/oa_login/oa_login.py
@@ -1,37 +1,89 @@
-import requests,base64
+# coding=utf-8
+import base64, requests
+import rsa, urllib
 from bs4 import BeautifulSoup
 
 
-def str2key(s):
-	# 对字符串解码
-	b_str = base64.b64decode(s)
- 
-	if len(b_str) < 162:
-		return False
- 
-	hex_str = ''
- 
-	# 按位转换成16进制
-	for x in b_str:
-		h = hex(ord(x))[2:]
-		h = h.rjust(2, '0')
-		hex_str += h
- 
-	# 找到模数和指数的开头结束位置
-	m_start = 29 * 2
-	e_start = 159 * 2
-	m_len = 128 * 2
-	e_len = 3 * 2
- 
-	modulus = hex_str[m_start:m_start + m_len]
-	exponent = hex_str[e_start:e_start + e_len]
- 
-	return modulus,exponent
+__all__ = ['rsa_encrypt']
+
+
+def _str2key(s):
+    # 对字符串解码
+    b_str = base64.b64decode(s)
+
+    if len(b_str) < 162:
+        return False
+
+    hex_str = ''
+
+    # 按位转换成16进制
+    for x in b_str:
+        h = hex(x)[2:]
+        h = h.rjust(2, '0')
+        hex_str += h
+
+    # 找到模数和指数的开头结束位置
+    m_start = 29 * 2
+    e_start = 159 * 2
+    m_len = 128 * 2
+    e_len = 3 * 2
+
+    modulus = hex_str[m_start:m_start + m_len]
+    exponent = hex_str[e_start:e_start + e_len]
+
+    return modulus, exponent
+
+
+def rsa_encrypt(s, pubkey_str):
+    '''
+    rsa加密
+    :param s:
+    :param pubkey_str:公钥
+    :return:
+    '''
+    key = _str2key(pubkey_str)
+    modulus = int(key[0], 16)
+    exponent = int(key[1], 16)
+    pubkey = rsa.PublicKey(modulus, exponent)
+    return base64.b64encode(rsa.encrypt(s.encode(), pubkey)).decode()
+
  
 if __name__ == "__main__":
+	s = requests.session()
 	bash_url = 'http://voa.grgbanking.com/'
-	response = requests.get(bash_url)
-	soup = BeautifulSoup(response.text,'lxml')
+	response = s.get(bash_url)
+	'''soup = BeautifulSoup(response.text,'lxml')
 	pubkey = soup.find('input',id='tra')['value'] 
-	key = str2key(pubkey)
-	print(key)
+	#key = urllib.parse.quote(rsa_encrypt('grg7792042', pubkey)).replace('+', '%2B')
+	key = rsa_encrypt('grg7792042', pubkey)
+	#print(key)
+	url = 'http://voa.grgbanking.com/HandlerGRGPortal.ashx'
+	payload = {
+		'method': 'getLoginTD',
+		'UserName': 'xjming9',
+		'UserKey': key,
+		'ValidateCode': '',
+		'NErrorCount': 0
+	}
+	response = s.post(url, data=payload)
+	#print(response.json())
+	url = response.json()['data']
+	resultData = s.get(url).json()['resultData']'''
+	payload = {
+	'method': 'GoToIndex',
+	'USER_ID': 'lping12',
+	'EMPID': 'G0108535',
+	'USER_NAME': '罗苹'
+	}
+	url = 'http://voa.grgbanking.com/HandlerGoToIndex.ashx'
+	response = s.post(url, data=payload)
+	url = 'http://voa.grgbanking.com/Index.aspx'
+	response = s.get(url)
+	soup = BeautifulSoup(response.text, 'lxml')
+	CheckString = soup.find('input', id='CheckString')['value']
+	print(CheckString)
+	sFlowURL = 'http://oa.grgbanking.com'+"/interface2/workflow_list2.php?"+CheckString+"&f=portal&type=0&size=5"
+	print(sFlowURL)
+	data = s.get(sFlowURL)
+	print(data.json())
+	
